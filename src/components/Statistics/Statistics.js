11d1c75e7cb0782afb226799134ecdfaf760c8b1
atik94 <atikurrahmanatik94@gmail.com>
2022-10-12 18:23:11 +0600

    Recharts responsibeness fixed

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,14 +5,16 @@ const Statistics = () => {
   const data = useLoaderData().data;
   console.log(data);
   return (
-    <div className="bg-dark ps-5 container">
-      <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="total" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+    <div className="bg-dark container mt-5">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };

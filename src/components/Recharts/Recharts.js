cd77ee37221a57bf4156920f5d035c7f5a06e0e5
atik94 <atikurrahmanatik94@gmail.com>
2022-10-12 12:33:14 +0600

    Recharts add

---
+++ src/components/Recharts/Recharts.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+const Recharts = ({ quiz }) => {
+  const { total } = quiz;
+  //console.log(total);
+  return <div></div>;
+};
+
+export default Recharts;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { useLoaderData } from "react-router-dom";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import Recharts from "../Recharts/Recharts";
 const Statistics = () => {
+  const data = useLoaderData().data;
+  console.log(data);
+  //const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
   return (
-    <div>
-      <h2>This is statistics</h2>
+    <div className="bg-dark ps-5 container">
+      <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
     </div>
   );
 };
